eaea5e57299dccf0c3a620341a1fdbd471a0a2df
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-05-03 16:24:04 +0200

    Add #fibs_recursive & #fibs
    The Fibonacci sequences have been finished.

---
+++ lib/recursive_fibonacci.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+def fibs(number)
+  fibonacci_sequence = [0, 1]
+  number.times do |index|
+    fibonacci_sequence << fibonacci_sequence[index] + fibonacci_sequence[index + 1]
+  end
+  puts fibonacci_sequence[0...number]
+end
+
+def fibs_recursive(number)
+  # if number is at 0 or 1, return number
+  if number == 0
+    return [0]
+  elsif number == 1
+    return [0, 1]
+  else 
+    fibonacci_array = fibs_recursive(number - 1) # Decrement number until it’s at 1 or 0
+    fibonacci_array << fibonacci_array[-1] + fibonacci_array[-2] # Add last two values and pushes them into the array
+  end
+end
