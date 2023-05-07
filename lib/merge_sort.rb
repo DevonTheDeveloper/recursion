fc673142ad4652620aef43887c2e4aa349dba155
Devon <dg.qndt+github@gmail.com>
2023-05-07 14:50:56 +0200

    Add #merge method
    This method allows the arrays to merge and compare themselves.

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
 def merge_sort(array)
-  if array.length <= 2
-    return unless array[0] > array[1]
-
-    array[1], array[0] = array[0], array[1]
+  if array.length < 2
     array
   else
-    left_half, right_half = merge_sort(array.each_slice((array.length / 2.0).round).to_a)
+    left_half = merge_sort(array.slice!(0, (array.length / 2.0).round))
+    right_half = merge_sort(array)
+    merge(left_half, right_half)
   end
+end
+
+def merge(left_half, right_half)
+  merged_array = []
+
+  until left_half.empty? || right_half.empty?
+    if left_half[0] <= right_half[0]
+      merged_array << left_half.shift
+    else
+      merged_array << right_half.shift
+    end
+  end
+  merged_array + left_half + right_half
 end
