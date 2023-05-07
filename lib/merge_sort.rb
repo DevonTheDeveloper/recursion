5d83c5019fda3ffc96310e2db4e7713d9416c8fb
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-05-07 14:49:20 +0200

    Split arrays in another way

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 def merge_sort(array)
-  if array.length <= 2
-    return unless array[0] > array[1]
-
-    array[1], array[0] = array[0], array[1]
+  if array.length < 2
     array
   else
-    left_half, right_half = merge_sort(array.each_slice((array.length / 2.0).round).to_a)
+    left_half = merge_sort(array.slice!(0, (array.length / 2.0).round))
+    right_half = merge_sort(array)
   end
 end
